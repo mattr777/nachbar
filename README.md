97def52cf6328a48b4b856be46656a1077ed4521
Matt Richards <bitbucket@richards777.com>
2015-06-13 00:40:44 -0600

    More progress.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Neighborhood Map
+
+## To run this app
+* Open [Matt's home page](http://richards777.com/ "Matt's Home Page") with a web browser.
+* A map will be shown displaying the neighborhood around Fluke Networks where Matt works.
+* Markers on the map indicate interesting places. A list of these is also shown on the left side of the screen.
+* Click on a marker to see more information. The list items are also clickable.
+* A search box is provided to search the places by name or category.
--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
  * Created by Matt on 6/11/2015.
  */
 function initialize() {
+    /**
+     * Create the map
+     */
     var mapOptions = {
         center: new google.maps.LatLng(38.9597, -104.7915),
         zoom: 17
@@ -12,6 +15,10 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
+    /**
+     * Create an array of interesting places
+     * @type {*[]}
+     */
     var markers = [{
         position: new google.maps.LatLng(38.9587, -104.7919),
         map: map,
@@ -30,13 +37,26 @@ function initialize() {
         {
             position: new google.maps.LatLng(38.9627, -104.7960),
             map: map,
-            title: 'Good Food'
+            title: 'Bird Dog BBQ'
+        },
+        {
+            position: new google.maps.LatLng(38.9626, -104.7945),
+            map: map,
+            title: 'Panera'
         }];
+
+    /**
+     * Create and bind the ViewModel for knockout
+     * @constructor
+     */
+    var ViewModel = function () {
+        this.markers = ko.observableArray(markers);
+    };
+    ko.applyBindings(new ViewModel());
 
     var bounds = new google.maps.LatLngBounds();
     var markersLength = markers.length;
     for (var i = 0; i < markersLength; i++) {
-        console.log(markers[i].position);
         bounds.extend(markers[i].position);
         var marker = new google.maps.Marker(markers[i]);
     }
